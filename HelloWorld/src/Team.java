ef4a6beb40c356aaf503af6ee0fd388a005451a1
ev20005 <eugene@genwiki.nl>
2017-04-20 21:31:28 +0200

    numberOfCyclists was not used

--- HelloWorld/src/Team.java
+++ HelloWorld/src/Team.java
@@ -14,17 +14,30 @@ public class Team {
  	Team(String name, int numberOfCyclists, String teamLeader){
 		this.name = name;
 		this.teamLeader = teamLeader;
+		this.numberOfCyclists = numberOfCyclists;
 	}
 	
-	public String getName() {
+	String getName() {
 	       return name;
 	}
 	
-	public String getTeamLeader() {
+	String getTeamLeader() {
 	       return teamLeader;
 	}
 	
-	public int getNumberOfCyclists() {
+	int getNumberOfCyclists() {
 	       return numberOfCyclists;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setNumberOfCyclists(int numberOfCyclists) {
+		this.numberOfCyclists = numberOfCyclists;
+	}
+
+	public void setTeamLeader(String teamLeader) {
+		this.teamLeader = teamLeader;
 	}
 }
